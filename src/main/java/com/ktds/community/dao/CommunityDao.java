b5f7a87b8ae04c755b3678c445a1e9a50733de3f
Admin-PC\Admin <kyend1@naver.com>
2018-03-21 10:09:36 +0900

    초기화

---
+++ src/main/java/com/ktds/community/dao/CommunityDao.java
@@ -0,0 +1,34 @@
+package com.ktds.community.dao;
+
+import java.util.List;
+
+import com.ktds.community.vo.CommunityVO;
+
+public interface CommunityDao {
+
+	public List<CommunityVO> selectAll();
+
+	public CommunityVO selectOne(int id);
+
+	public int selectMyCommunitiesCount(int userId);
+
+	public List<CommunityVO> selectMyCommunities(int userId);
+
+	public int insertCommunity(CommunityVO communityVO);
+
+	public int incrementViewCount(int id);
+
+	public int incrementRCount(int id);
+
+	public int deleteOne(int id);
+
+	public int deleteCommunities(List<Integer> ids, int userId);
+	
+	
+	public int deleteMyCommunities(int userId);
+
+	
+	
+	public int updateCommunity(CommunityVO communityVO);
+
+}
